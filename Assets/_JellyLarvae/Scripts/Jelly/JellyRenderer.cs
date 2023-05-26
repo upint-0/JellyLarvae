c97e42eadc6715e55722cb16645d86ef8f5ebbc7
D D L <damibolo@gmail.com>
2023-05-27 01:26:02 +0200

    - resolve amplify -

--- Assets/_JellyLarvae/Scripts/Jelly/JellyRenderer.cs
+++ Assets/_JellyLarvae/Scripts/Jelly/JellyRenderer.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class JellyRenderer : MonoBehaviour
 {
-    [SerializeField] private Vector2 _MapSize = Vector2.one;
+    [SerializeField] private Vector2Int _MapSize = Vector2Int.one;
 
-    [Header("Compute")] [SerializeField] 
-    private ComputeShader _ComputeShaderJellyMask;
-    private Material _JellyRenderer;
+    [Header("Compute")] 
+    [SerializeField] private ComputeShader _ComputeShaderJellyMask;
+    [SerializeField] private Material _JellyRenderer;
     
     [Header("Texture settings")]
     [SerializeField] [Range(0.1f,10f)]private float _TextureQuality = 1f;
     [SerializeField] private RenderTexture _JellyMask;
     
-    
+    public void SetupRenderTexture()
+    {
+        Vector2Int size = new Vector2Int((int) (_MapSize.x * _TextureQuality), (int) (_MapSize.y * _TextureQuality));
+        _JellyMask.width = size.x;
+        _JellyMask.height = size.y;
+    }
     
 }
