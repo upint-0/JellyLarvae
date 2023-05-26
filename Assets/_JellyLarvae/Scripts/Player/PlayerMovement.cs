d3c39eff15477bd83a2a38ecada5d009e8584e5a
Pierre De Laet <pierre.de.laet@fishingcactus.be>
2023-05-27 01:23:07 +0200

    adding naughty attributes

--- Assets/_JellyLarvae/Scripts/Player/PlayerMovement.cs
+++ Assets/_JellyLarvae/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float Speed = 1.0f;
+    [SerializeField] private float StopDistance = 1.0f;
+
+    private Rigidbody2D _rigidbody2D;
+    Vector2 _direction = new Vector2();
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        float distance_to_mouse = _direction.magnitude;
+        _direction = _direction.normalized;
+
+        float z_rotation = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, z_rotation);
+    
+        if (Input.GetMouseButton(0) && distance_to_mouse > StopDistance)
+        {
+            transform.position += transform.right * Speed * Time.deltaTime;
+        }
+
     }
 }
