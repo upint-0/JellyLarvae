f2efe5c7e67badf06c7caf9ea6977a9c9511afec
D D L <damibolo@gmail.com>
2023-05-27 02:05:34 +0200

    - begin jelly mask -

--- Assets/_JellyLarvae/Scripts/Jelly/JellyRenderer.cs
+++ Assets/_JellyLarvae/Scripts/Jelly/JellyRenderer.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
+using static UnityExtensions;
 
 public class JellyRenderer : MonoBehaviour
 {
@@ -8,17 +11,64 @@ public class JellyRenderer : MonoBehaviour
 
     [Header("Compute")] 
     [SerializeField] private ComputeShader _ComputeShaderJellyMask;
-    [SerializeField] private Material _JellyRenderer;
+    [SerializeField] private SpriteRenderer _JellyRenderer;
+    private Material _MaterialInstance;
+    private int _MainKernel;
+    private Vector3Uint _KernelGroupeSize = new Vector3Uint();
     
     [Header("Texture settings")]
     [SerializeField] [Range(0.1f,10f)]private float _TextureQuality = 1f;
     [SerializeField] private RenderTexture _JellyMask;
+
+    private MaterialPropertyBlock _MaterialPropertyBlock;
     
-    public void SetupRenderTexture()
+    [Button("Setup Render Texture")]
+    void SetupRenderTexture()
     {
         Vector2Int size = new Vector2Int((int) (_MapSize.x * _TextureQuality), (int) (_MapSize.y * _TextureQuality));
         _JellyMask.width = size.x;
         _JellyMask.height = size.y;
     }
-    
+
+    private void OnEnable()
+    {
+        Init();
+    }
+
+    private void OnDisable()
+    {
+        _JellyMask.Release();
+    }
+
+    private void Init()
+    {
+        Vector2Int size = new Vector2Int((int) (_MapSize.x * _TextureQuality), (int) (_MapSize.y * _TextureQuality));
+
+        _JellyMask = new RenderTexture(size.x, size.y, 16, RenderTextureFormat.ARGB32);
+        //_JellyMask.Create();
+        _JellyMask.enableRandomWrite = true;
+        
+        _MaterialPropertyBlock = new MaterialPropertyBlock();
+        _MaterialPropertyBlock.SetTexture("_MainTex", _JellyMask);
+        
+        _JellyRenderer.SetPropertyBlock(_MaterialPropertyBlock);
+
+        
+
+
+        _MainKernel = _ComputeShaderJellyMask.FindKernel("CSMain");
+        _ComputeShaderJellyMask.GetKernelThreadGroupSizes(_MainKernel, out _KernelGroupeSize.x,out _KernelGroupeSize.y, out _KernelGroupeSize.z);
+        
+    }
+
+    private void Update()
+    {
+        Draw();
+        _ComputeShaderJellyMask.SetTexture(_MainKernel,"_JellyMask", _JellyMask);
+        _ComputeShaderJellyMask.Dispatch(_MainKernel, (int) _KernelGroupeSize.x, (int) _KernelGroupeSize.y,(int) _KernelGroupeSize.z);
+    }
+
+    private void Draw()
+    {
+    }
 }
---
+++ Assets/_JellyLarvae/Scripts/UnityExtensions.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityExtensions : MonoBehaviour
+{
+    public class Vector3Uint
+    {
+        public uint x, y, z;
+
+        public Vector3Uint()
+        {
+            
+        }
+    }
+}
