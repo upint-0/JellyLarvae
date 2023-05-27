3cd42b91d9c5f36aaf4a4347f765dd7a84556da0
D D L <damibolo@gmail.com>
2023-05-27 02:59:30 +0200

    try to draw mouse position on the render texture

--- Assets/_JellyLarvae/Scripts/Jelly/JellyRenderer.cs
+++ Assets/_JellyLarvae/Scripts/Jelly/JellyRenderer.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
+using UnityEditor;
 using UnityEngine;
 using static UnityExtensions;
 
 public class JellyRenderer : MonoBehaviour
 {
+    [SerializeField] private bool _UseSpriteScaleForSize = false;
     [SerializeField] private Vector2Int _MapSize = Vector2Int.one;
 
+    [Header("Paint")] 
+    [SerializeField] private float _BrushSize = 1f;
     [Header("Compute")] 
     [SerializeField] private ComputeShader _ComputeShaderJellyMask;
     [SerializeField] private SpriteRenderer _JellyRenderer;
@@ -52,13 +56,14 @@ public class JellyRenderer : MonoBehaviour
         _MaterialPropertyBlock.SetTexture("_MainTex", _JellyMask);
         
         _JellyRenderer.SetPropertyBlock(_MaterialPropertyBlock);
-
+        
         
 
 
         _MainKernel = _ComputeShaderJellyMask.FindKernel("CSMain");
         _ComputeShaderJellyMask.GetKernelThreadGroupSizes(_MainKernel, out _KernelGroupeSize.x,out _KernelGroupeSize.y, out _KernelGroupeSize.z);
         
+        _ComputeShaderJellyMask.SetVector("_TextureSize", new Vector2(size.x, size.y));
     }
 
     private void Update()
@@ -66,9 +71,29 @@ public class JellyRenderer : MonoBehaviour
         Draw();
         _ComputeShaderJellyMask.SetTexture(_MainKernel,"_JellyMask", _JellyMask);
         _ComputeShaderJellyMask.Dispatch(_MainKernel, (int) _KernelGroupeSize.x, (int) _KernelGroupeSize.y,(int) _KernelGroupeSize.z);
+        
+        _MaterialPropertyBlock.SetTexture("_MainTex", _JellyMask);
     }
 
     private void Draw()
     {
+        // Canvas
+        Vector2 spriteSize = _JellyRenderer.transform.localScale;
+        Vector2 size = (_UseSpriteScaleForSize) ? spriteSize : _MapSize;
+        _MapSize.x = (int)size.x;
+        _MapSize.y = (int)size.y;
+        Vector4 canvasSettings = new Vector4(_JellyRenderer.transform.position.x, _JellyRenderer.transform.position.y,
+            size.x, size.y);
+        
+        _ComputeShaderJellyMask.SetVector("_CanvasSettings", canvasSettings);
+        
+        // Draw with mouse
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 mousePosWS = Camera.main.ScreenToWorldPoint(mousePos);
+
+        bool isDrawing = Input.GetMouseButton(0);
+        Vector4 brushSettings = new Vector4(mousePosWS.x, mousePosWS.y, _BrushSize, Convert.ToSingle(isDrawing));
+        
+        _ComputeShaderJellyMask.SetVector("_BrushSettings", brushSettings);
     }
 }
