ffb5691c0c774568278e1423119e1df36da15b6a
Pierre De Laet <pierre.de.laet@fishingcactus.be>
2023-06-02 00:38:45 +0200

    eating attributes

--- Assets/_JellyLarvae/Scripts/Player/PlayerMovement.cs
+++ Assets/_JellyLarvae/Scripts/Player/PlayerMovement.cs
@@ -70,7 +70,9 @@ public class PlayerMovement : MonoBehaviour
     private bool DetectJelly()
     {
         //return Physics2D.OverlapCircle( transform.position, 1.0f, LayerMask.GetMask( "Wall" ) );
-        return (Jelly.GetJellyValueAtPosition( transform.position, true, transform.position, 50f) > 0.1f);
+        var position = transform.position;
+        Debug.DrawRay(position - (transform.right * PlayerAttributes.EatOffset), Vector3.up, Color.blue);
+        return (Jelly.GetJellyValueAtPosition( position, true, position - (transform.right * PlayerAttributes.EatOffset), PlayerAttributes.EatRadius) > 0.1f);
     }
 
     private void HandleDash()
